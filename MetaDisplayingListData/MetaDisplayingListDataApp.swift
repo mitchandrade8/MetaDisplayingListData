6350b70468a79e0c33f9379c192fb53d9e37f2a2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-30 09:09:05 -0600

    Initial Commit

---
+++ MetaDisplayingListData/MetaDisplayingListDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaDisplayingListDataApp.swift
+//  MetaDisplayingListData
+//
+//  Created by Mitch Andrade on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaDisplayingListDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
